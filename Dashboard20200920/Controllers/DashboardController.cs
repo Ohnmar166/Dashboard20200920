aaa5b9507c3d53080ae4dc0224f7ab91a14dbc17
Ohnmar166 <ohnmarlay888@gmail.com>
2020-09-29 22:14:56 +0630

    first commit

---
+++ Dashboard20200920/Controllers/DashboardController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dashboard20200920.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Clock()
+        {
+            return View();
+        }
+        public ActionResult HeatMap()
+        {
+            return View();
+        }
+        public ActionResult CandleStickSeries()
+        {
+            return View();
+        }
+        public ActionResult ChordDiagram()
+        {
+            return View();
+        }
+        public ActionResult BubbleChart()
+        {
+            return View();
+        }
+        public ActionResult ChangingSeriesAppearance()
+        {
+            return View();
+        }
+        public ActionResult ChangingTreeMapData()
+        {
+            return View();
+        }
+        public ActionResult ChordDiagramNonRibbon()
+        {
+            return View();
+        }
+        public ActionResult ChordFriendsKisses()
+        {
+            return View();
+        }
+        //ZingChard
+        public ActionResult ColorfulPieChart()
+        {
+            return View();
+        }
+        public ActionResult PopulationPyramidAnimation()
+        {
+            return View();
+        }
+        //GalleryG2
+        public ActionResult ColumnChart()
+        {
+            return View();
+        }
+        public ActionResult LineChart()
+        {
+            return View();
+        }
+        public ActionResult AreaChart()
+        {
+            return View();
+        }
+        public ActionResult MapChart()
+        {
+            return View();
+        }
+        public ActionResult PolarVonoroi()
+        {
+            return View();
+        }
+        //ApexCharts
+        public ActionResult RadiaBar()
+        {
+            return View();
+        }
+        public ActionResult Area()
+        {
+            return View();
+        }
+        public ActionResult StrokedGauge()
+        {
+            return View();
+        }
+        public ActionResult CircleChart()
+        {
+            return View();
+        }
+        //Chart-js
+        public ActionResult PointSizes()
+        {
+            return View();
+        }
+        public ActionResult Vertical()
+        {
+            return View();
+        }
+    }
+}
---
+++ Dashboard20200920/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dashboard20200920.MvcApplication" Language="C#" %>
